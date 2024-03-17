814986694dbfb0c2c7a32f4a060c274e5f91cc5b
Aliona Kosachova <alionakosachova@gmail.com>
2024-03-17 22:57:29 +0200

    Task10_new

---
+++ BasicAuthontificator.cs
@@ -0,0 +1,43 @@
+﻿using APIAutomation;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace APIAutomation
+{
+    public class BasicAuthenticator : AuthenticatorBase
+    {
+        readonly string _baseUrl;
+        readonly string _clientUsername;
+        readonly string _clientPassword;
+        readonly string _scope;
+
+        public BasicAuthenticator(string baseUrl, string clientUsername, string clientPassword, string scope) : base("")
+        {
+            _baseUrl = baseUrl;
+            _clientUsername = clientUsername;
+            _clientPassword = clientPassword;
+            _scope = scope;
+        }
+
+        protected override async ValueTask<Parameter> GetAuthenticationParameter(string accessToken)
+        {
+            Token = string.IsNullOrEmpty(Token) ? await GetToken() : Token;
+            return new HeaderParameter(KnownHeaders.Authorization, Token);
+        }
+
+        public async Task<string> GetToken()
+        {
+            var options = new RestClientOptions(_baseUrl)
+            {
+                Authenticator = new HttpBasicAuthenticator(_clientUsername, _clientPassword),
+            };
+            using var client = new RestClient(options);
+
+            var request = new RestRequest("oauth/token").AddParameter("grant_type", "client_credentials");
+            request.AddParameter("scope", _scope);
+            var response = await client.PostAsync<TokenResponse>(request);
+            return $"{response!.TokenType} {response!.AccessToken}";
+        }
+    }
+}
---
+++ RestClients.cs
@@ -0,0 +1,92 @@
+﻿using APIAutomation;
+using NUnit.Framework;
+using RestSharp;
+
+namespace APIAutomation
+{
+    public class ClientForWriteScope
+    {
+        private static ClientForWriteScope _instance;
+        private static readonly object _lock = new object();
+        private readonly RestClient _client;
+
+        private ClientForWriteScope(string baseURL, string clientUsername, string clientPassword)
+        {
+            var options = new RestClientOptions(baseURL)
+            {
+                Authenticator = new BasicAuthenticator(baseURL, clientUsername, clientPassword, "write")
+            };
+
+            _client = new RestClient(options);
+        }
+
+        public static ClientForWriteScope GetInstance(string baseURL, string clientUsername, string clientPassword)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ClientForWriteScope(baseURL, clientUsername, clientPassword);
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public RestClient GetRestClient()
+        {
+            return _client;
+        }
+
+    }
+
+    public class ClientForReadScope
+    {
+        private static ClientForReadScope _instance;
+        private static readonly object _lock = new object();
+        private readonly RestClient _client;
+
+        private ClientForReadScope(string baseURL, string clientUsername, string clientPassword)
+        {
+            var options = new RestClientOptions(baseURL)
+            {
+                Authenticator = new BasicAuthenticator(baseURL, clientUsername, clientPassword, "read")
+            };
+
+            _client = new RestClient(options);
+        }
+
+        public static ClientForReadScope GetInstance(string baseURL, string clientUsername, string clientPassword)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ClientForReadScope(baseURL, clientUsername, clientPassword);
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public RestClient GetRestClient()
+        {
+            return _client;
+        }
+    }
+}
+
+//Use for Debugging. To delete after tests implementation
+public static class Test_For_Auth_Check
+{
+    [Test]
+    public static void Test1()
+    {
+        var client = ClientForWriteScope.GetInstance("http://localhost:50990", "0oa157tvtugfFXEhU4x7", "X7eBCXqlFC7x-mjxG5H91IRv_Bqe1oq7ZwXNA8aq");
+        var restClient = client.GetRestClient();
+    }
+}
