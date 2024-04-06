aa21339d6f91c7e2220ce3a726e7810be17dceb2
Aliona Kosachova <alionakosachova@gmail.com>
2024-04-06 11:56:10 +0300

    Task40_improvement

--- User.cs
+++ User.cs
@@ -16,4 +16,17 @@
             return false;
         return  Name == other.Name && Age == other.Age && Sex == other.Sex && ZipCode == other.ZipCode;
     }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 23 + Name.GetHashCode();
+            hash = hash * 23 + (Age != null ? Age.GetHashCode() : 0);
+            hash = hash * 23 + Sex.GetHashCode();
+            hash = hash * 23 + (ZipCode != null ? ZipCode.GetHashCode() : 0);
+            return hash;
+        }
+    }
 }
